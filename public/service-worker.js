8d0e76ae8e7730a1f94fcae21dca1580edd1d244
alfinro <romednetsrl@gmail.com>
2025-10-29 21:13:30 +0200

    Add files via upload
    actualizare notificări - buton notificare pe mobil

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,1 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker installed");
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  console.log("Service Worker activated");
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
-});
+// service worker
